b00e8a256796fd65729ddff1bcf7142462940a43
cabralcm <christophercabral@gmail.com>
2020-05-27 22:53:21 -0400

    yes

---
+++ p1/strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string charName = "Julian";
+    int charAge = 500;
+
+    cout << "There once was a man named " <<charName << endl;
+    cout << "He wasn't old at all, a mere " << charAge << " Years old" << endl;
+    cout << "The End." << endl;
+
+}
